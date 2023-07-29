f32f3fd68bd63bfc89c3ae1b775d1efb2a87e66d
JoeMiran <joemiran06@gmail.com>
2023-07-29 13:25:42 -0300

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed:

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
       			<div class="col-md-12">
   				
 					<div class="card-body font-weight-bold">
-						<form>
+						<form action="processa_envio.php" method='POST'>
 							<div class="form-group">
 								<label for="para">Para</label>
 								<input type="text" class="form-control" id="para" placeholder="exemplo@email.com">
