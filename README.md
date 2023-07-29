9cef03bbe4fefa87b4f2b81218fdf9eaa48bd23d
JoeMiran <joemiran06@gmail.com>
2023-07-29 19:46:34 -0300

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed:

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # app_send_mail
 🚀Aplicação:
 - 
-- Esse é uma aplicação simples e usual, desenvolvida em PHP para enviar emails através do serviço de SMTP do Gmail.
+*$this*-> Esse é uma aplicação simples e usual, desenvolvida em PHP para enviar emails através do serviço de SMTP do Gmail sem que seja ne.
 
 🎯Objetivos:
 -
@@ -10,3 +10,7 @@
 - Treinar a interação do Front-End com o Back-End da aplicação.
 - Por em prática a utilização das classes do Bootstrap para estilização do projeto.
 - Praticar tratamentos de erros e segurança do lado do Back-End para proteger arquivos sigilosos.
+
+📨Como usar:
+-
+*$this*-> Para usar a aplicação, você irá precisar configurar uma conta de email pela qual você fará os envios, pois é necessário que exista um usuário gmail ou de qualquer outro serviço que utilize o protocolo SMTP. Para fazer isso, basta acessar o código no arquivo "processa_envio.php" e configurar o SMTP username e SMTP password nas linhas 60 e 61 respectiviamente. Lembre-se de nunca divulgar sua senha caso seja solicitada indevidamente por quem quer que seja, por está razão estou utilizando um email de teste.
--- processa_envio.php
+++ processa_envio.php
@@ -53,12 +53,12 @@ if(!$mensagem->mensagemValida()) { //Verificando se a mensagem é válida
 $mail = new PHPMailer(true);
 try {
         //Server settings
-        $mail->SMTPDebug = 2;                      //Enable verbose debug output
+        $mail->SMTPDebug = 2;                                       //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'user@example.com';                     //SMTP username
-        $mail->Password   = 'secret';                               //SMTP password
+        $mail->Username   = 'testeemai876@gmail.com';               //SMTP username
+        $mail->Password   = 't3st&senh@';                           //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
