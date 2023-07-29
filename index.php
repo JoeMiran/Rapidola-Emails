f72db96d644ba34c80371b0482999eed4a13d1f7
JoeMiran <joemiran06@gmail.com>
2023-07-29 12:03:38 -0300

    On branch main  Initial commit  Changes to be committed:

---
+++ index.php
@@ -0,0 +1,48 @@
+<html>
+	<head>
+		<meta charset="utf-8" />
+    	<title>Rapidola Email</title>
+
+    	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+	</head>
+
+	<body>
+
+		<div class="container">  
+
+			<div class="py-3 text-center">
+				<img class="d-block mx-auto mb-2" src="logo.png" alt="" width="72" height="72">
+				<h2>Rapidola</h2>
+				<p class="lead">Enviei seu email de forma mais objetiva com o Rapidola email</p>
+			</div>
+
+      		<div class="row">
+      			<div class="col-md-12">
+  				
+					<div class="card-body font-weight-bold">
+						<form>
+							<div class="form-group">
+								<label for="para">Para</label>
+								<input type="text" class="form-control" id="para" placeholder="exemplo@email.com">
+							</div>
+
+							<div class="form-group">
+								<label for="assunto">Assunto</label>
+								<input type="text" class="form-control" id="assunto" placeholder="Assundo do e-mail">
+							</div>
+
+							<div class="form-group">
+								<label for="mensagem">Mensagem</label>
+								<textarea class="form-control" id="mensagem"></textarea>
+							</div>
+
+							<button type="submit" class="btn btn-primary btn-lg">Enviar</button>
+						</form>
+					</div>
+				</div>
+      		</div>
+      	</div>
+
+	</body>
+</html>
